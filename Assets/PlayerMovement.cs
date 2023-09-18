02353a24789c8abb22506eeeb0f86806663cc5d7
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-09-17 17:36:53 -0700

    Created new player movement script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] InputAction movement;
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
