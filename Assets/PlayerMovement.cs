dd9e66062c6299300702c950a0a495f508990623
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-09-17 18:05:29 -0700

    Add Jump
    Uses physics based movement instead of transform

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,22 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] InputAction movement;
+    [SerializeField] InputAction jump;
     [SerializeField][Range(0,0.5f)] float speedFactor;
+    [SerializeField][Min(0)] float jumpFactor;
+    Rigidbody rb;
+
 
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
     {
         movement.Enable();
+        jump.Enable();
     }
 
     private void OnDisable()
     {
         movement.Disable();
+        jump.Disable();
     }
 
     // Update is called once per frame
@@ -31,6 +37,10 @@ public class PlayerMovement : MonoBehaviour
         if (movement != null)
         {
             transform.position = new Vector3(transform.position.x + movement.ReadValue<float>() * speedFactor, transform.position.y);
+        }
+        if (jump != null)
+        {
+            rb.AddForce(new Vector3(0, jumpFactor, 0), ForceMode.Impulse);
         }
     }
 }
