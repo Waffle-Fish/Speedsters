f037f5f417e405eb849a6bac8fcd294e0f7f7539
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-09-26 23:39:28 -0700

    Revert "Merge branch 'main' into Josues_branch"
    This reverts commit 9744202b90946f9df5731bbd1939526f48f1c69a, reversing
    changes made to ffe217e78de9b243300cd9d2e93fdb76aef82bc1.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] InputAction movement;
     [SerializeField] InputAction jump;
-    [SerializeField] InputAction crouch;                 // make this down button while on ground
-    [SerializeField] InputAction fastFall;               // make this down button while in air
-    [SerializeField] [Range(0, 0.5f)] float speedFactor;
-    [SerializeField] [Min(0)] float jumpFactor;
-    [SerializeField] [Min(0)] float doublejumpFactor;
-    [SerializeField] [Min(0)] float fastFallFactor;
+    [SerializeField][Range(0,0.5f)] float speedFactor;
+    [SerializeField][Min(0)] float jumpFactor;
     Rigidbody2D rb;
 
-    // checks
-    public bool isOnGround;
-    public bool canDoubleJump;
-    public bool canFastFall;
-    //------------------------------------------------------
+
 
     void Start()
     {
@@ -42,59 +34,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //left right movement
         if (movement != null)
         {
             transform.position = new Vector3(transform.position.x + movement.ReadValue<float>() * speedFactor, transform.position.y);
         }
-
-        //ground jump
-        if (jump.WasPressedThisFrame() && isOnGround)
+        if (jump.WasPressedThisFrame())
         {
             rb.AddForce(new Vector2(0, jump.ReadValue<float>() * jumpFactor), ForceMode2D.Impulse);
-        }
-
-        //double jump
-        if (jump.WasPressedThisFrame() && !isOnGround && canDoubleJump)
-        {
-            rb.AddForce(new Vector2(0, jump.ReadValue<float>() * doublejumpFactor), ForceMode2D.Impulse);
-            canDoubleJump = false;
-            canFastFall = true;
-
-        }
-
-//crouching button check
-        if (crouch.WasPressedThisFrame() && isOnGround)
-        {
-            //crouching code
-        }
-//Fast fall button check
-        if (crouch.WasPressedThisFrame() && !isOnGround && canFastFall) // fastfall should be possible once per leaving ground not "WasPressedThisFrame()" 
-        {
-            //fast falling code
-            //rb.AddForce(new Vector2(0, fastFall.ReadValue<float>() * fastFallFactor), ForceMode2D.Impulse);
-            //canFastFall = false;
-
-        }
-    }
-
-    //after landing on ground
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-            canDoubleJump = true;
-            canFastFall = false;
-        }
-    }
-    //after exiting ground 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = false;
-            canFastFall = true;
         }
     }
 }
