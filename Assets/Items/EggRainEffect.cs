b6fc141d9c7c4fad3bb1055fa7da0ea26ad0fb97
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-10-05 20:07:38 -0700

    Add EggRain

---
+++ Assets/Items/EggRainEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggRainEffect : ItemCore
+{
+    [SerializeField]
+    private float timer;
+    public override void ActivateEffect()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+}
