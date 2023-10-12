993637dc402ca3d48a45f11d95c9c366dfc466d8
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-10-11 20:06:24 -0700

    Update Egg

--- Assets/Items/EggRain/EggRainEffect.cs
+++ Assets/Items/EggRain/EggRainEffect.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 // TODO:
-// Have player detect being hit by egg
+// Fix Egg Behavior Hitting Player
+// Fix Destruction of Instantiated Eggs and Cloud
 // Create Splatter effect on sceen
 // Reduce player speed
 // Change layer ordering of egg to cloud
@@ -28,8 +29,14 @@ public class EggRainEffect : ItemCore
     [SerializeField][Min(0.01f)][Tooltip("How long it takes for an egg from cloud to player")]
     private float eggDropDuration = 0f;
     private List<GameObject> eggList = new();
+    private int eggCounter = 0;
 
     private void Start()
+    {
+        SpawnEntities();
+    }
+
+    private void SpawnEntities()
     {
         cloud = Instantiate(cloud, transform.position, cloud.transform.rotation, this.transform);
         for (int i = 0; i < totalEggSpawn; i++)
@@ -46,27 +53,23 @@ public class EggRainEffect : ItemCore
         StopTimer();
         ResetTimer();
         StartTimer();
-        int eggCounter = 1;
         SpawnCloud();
         while (timer <= effectDuration)
         {
             cloud.GetComponentInParent<Transform>().position = new Vector2(enemy.transform.position.x, enemy.transform.position.y + cloudHeight);
-            if (timer >= eggCounter * effectDuration / totalEggSpawn)
+            if (timer > (eggCounter + 1) * effectDuration / totalEggSpawn)
             {
-                //Spawn egg
-                UnityEngine.Debug.Log("Egg " + eggCounter + " spawned - Time: " + timer);
-                eggList[eggCounter-1].SetActive(true);
-                StartCoroutine(RainEgg(eggList[eggCounter - 1]));
+                eggList[eggCounter].SetActive(true);
+                StartCoroutine(RainEgg(eggList[eggCounter]));
+                //UnityEngine.Debug.Log("Egg " + eggCounter + " spawned - Time: " + timer);
                 eggCounter++;
             }
             timer += Time.deltaTime;
             yield return null;
         }
-        UnityEngine.Debug.Log("All the eggs have finished being spawned");
-        StopTimer();
-        ResetTimer();
-        effectIsActive = false;
-        cloud.SetActive(false);
+        ProcessEnd();
+        //UnityEngine.Debug.Log("All the eggs have finished being spawned");
+
     }
 
     private IEnumerator RainEgg(GameObject egg)
@@ -77,10 +80,14 @@ public class EggRainEffect : ItemCore
         while (egg.transform.position.y > enemyTransform.position.y)
         {
             float dropRate = (cloudHeight / eggDropDuration) * Time.deltaTime;
-            UnityEngine.Debug.Log("DropRate: " + dropRate);
             float spawnPosY = egg.transform.position.y - dropRate;
             Vector2 eggPos = new Vector2(spawnPosX, spawnPosY);
             egg.transform.position = eggPos;
+            if (egg.GetComponent<EggBehavior>().HitPlayer)
+            {
+                ProcessHit();
+                break;
+            }
             yield return null;
         }
         egg.SetActive(false);
@@ -90,15 +97,24 @@ public class EggRainEffect : ItemCore
     {
         cloud.transform.position = new(enemyTransform.position.x, enemyTransform.position.y + cloudHeight);
         cloud.SetActive(true);
-        UnityEngine.Debug.Log("Cloud has spawned");
+        //UnityEngine.Debug.Log("Cloud has spawned");
+    }
+
+    private void ProcessHit()
+    {
+        UnityEngine.Debug.Log("Egg " + eggCounter + " has hit player " + enemy.tag);
+    }
+
+    private void ProcessEnd()
+    {
+        StopTimer();
+        ResetTimer();
+        effectIsActive = false;
+        cloud.SetActive(false);
     }
 
     private void OnDestroy()
     {
-        Destroy(cloud);
-        for (int i = 0; i < eggList.Count; i++)
-        {
-            Destroy(eggList[i]);
-        }
+        Destroy(this);
     }
 }
