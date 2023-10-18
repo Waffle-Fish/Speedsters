7758b8bde47ffa6730d5379abf0af463ae7baaeb
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-10-18 14:02:18 -0700

    Tried updating main menu

--- Assets/Scripts/Menu/mainMenu.cs
+++ Assets/Scripts/Menu/mainMenu.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class mainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    [Range(0, 10)]
+    private int sceneIndex;
     public void PlayGame()
     {
+        Debug.Log("Lets play!");
         SceneManager.LoadScene(1);
     }
     public void QuitGame()
