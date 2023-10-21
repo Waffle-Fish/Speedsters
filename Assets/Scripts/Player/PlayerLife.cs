618f47327a12851dbaa1463f323a4f4cbdc7dfb1
Josue <xJaramis@gmail.com>
2023-10-21 00:00:28 -0700

    CameraClamp and Timer/FinishLine Made

--- Assets/Scripts/Player/PlayerLife.cs
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+//Script that controls hitbox operations
 public class PlayerLife : MonoBehaviour
 {
     [SerializeField]
@@ -24,6 +23,42 @@ public class PlayerLife : MonoBehaviour
         {
             respawnPoint = transform.position;
             Debug.Log("Spawn Point Updated");
+        }
+        else if(collision.tag == "Finish")
+        {
+            if (this.tag == "Player1")          //Player 1 is finished and gets frozen 
+            {
+                Time_UI.Instance.isP1Finished = true;
+
+                //Removes momentum and disables PlayerController Script
+                PlayerController p1 = Player1Master.Instance.GetComponent<PlayerController>();
+
+                p1.setVel(new Vector2(0, 0));
+                p1.enabled = false;
+                
+                Debug.Log("Player 1 Finished");
+            }
+            else if(this.tag == "Player2")      //Player 2 is finished and gets frozen 
+            {
+                Time_UI.Instance.isP2Finished = true;
+                
+                //Removes momentum and disables PlayerController Script
+                PlayerController p2 = Player2Master.Instance.GetComponent<PlayerController>();
+                p2.setVel(new Vector2(0, 0));
+                p2.enabled = false;
+
+                Debug.Log("Player 2 Finished");
+            }
+
+            //Calls winEvent() if both finished
+            if (Time_UI.Instance.isP1Finished && Time_UI.Instance.isP2Finished)
+            {
+                winEvent();
+            }
+            
+            /* Freezes player and timer, uses check variable to see if both players have finished, if so, then player
+            with least time wins  -> sends to win screen with scoreboard maybe or menu screen? Freeze player function can be made
+            for start as well if we want a countdown maybe                                     */
         }
 
     }
@@ -49,5 +84,11 @@ public class PlayerLife : MonoBehaviour
         {
             transform.position = respawnPoint;
         }
+    }
+
+    private void winEvent()
+    {
+        //getWinner() returns char ('1' or '2')
+        Debug.Log("Winner is Player " + Time_UI.Instance.getWinner() + "!");
     }
 }
---
+++ Assets/Scripts/UI/Time_UI.cs
@@ -0,0 +1,75 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+public class Time_UI : MonoBehaviour
+{
+    public static Time_UI Instance;
+
+    //Timer GameObjects
+    [SerializeField]
+    GameObject time_P1;
+    [SerializeField]
+    GameObject time_P2;
+
+    float time = 0;
+    public bool isP1Finished = false;
+    public bool isP2Finished = false;
+
+    char finishFirst; 
+
+    TextMeshProUGUI text1;
+    TextMeshProUGUI text2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        //Connect text
+        text1 = time_P1.GetComponent<TextMeshProUGUI>();
+        text2 = time_P2.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isP1Finished && !isP2Finished)
+        {
+            time += Time.deltaTime;
+            text1.text = displayTime(time);
+            text2.text = text1.text;
+        }
+        else if (!isP1Finished && isP2Finished)
+        {
+            time += Time.deltaTime;
+            finishFirst = '2';
+            text1.text = displayTime(time);
+        }
+        else if (isP1Finished && !isP2Finished) 
+        { 
+            time += Time.deltaTime;
+            finishFirst = '1';
+            text2.text = displayTime(time);
+        }
+    }
+
+    string displayTime(float time)
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+        float milli = time % 1 * 1000;
+
+        return (string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milli));
+    }
+
+    public char getWinner()
+    {
+        return finishFirst;
+    }
+}
