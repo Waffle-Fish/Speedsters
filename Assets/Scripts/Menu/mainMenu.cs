46c1d3ef9404fbd445168dcff822b6be15615d0a
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-10-29 02:19:06 -0700

    Update PlayGame script

--- Assets/Scripts/Menu/mainMenu.cs
+++ Assets/Scripts/Menu/mainMenu.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [Min(0)]
-    public int sceneIndex;
-    public void PlayGame()
+    public void PlayGame(int sceneIndex)
     {
         Debug.Log("Lets play!");
         if(sceneIndex >= SceneManager.sceneCountInBuildSettings) { Debug.LogError("Trying to load a scene outside the current number of scenes in the build settings"); }
