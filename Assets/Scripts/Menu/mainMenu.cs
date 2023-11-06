4f0a5430f199fc71ea825372c6581b3021ad60ed
Josue <xJaramis@gmail.com>
2023-11-05 16:46:08 -0800

    Level Select and Character Select Scene

--- Assets/Scripts/Menu/mainMenu.cs
+++ Assets/Scripts/Menu/mainMenu.cs
@@ -14,6 +14,13 @@ public class MainMenu : MonoBehaviour
         if(sceneIndex >= SceneManager.sceneCountInBuildSettings) { Debug.LogError("Trying to load a scene outside the current number of scenes in the build settings"); }
         SceneManager.LoadScene(sceneIndex);
     }
+
+    //Make sure to know what scene index the levels areS
+    public void setIndex(int index)
+    {
+        sceneIndex = index;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
