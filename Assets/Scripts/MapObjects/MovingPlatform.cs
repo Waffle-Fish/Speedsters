a5b1f3972313c7a5d4396e25f8fbc88240777639
JesDaw <jd281003@gmail.com>
2023-11-08 10:59:05 -0800

    Added moving platforms
    Added moving platforms and placed some in the map

---
+++ Assets/Scripts/MapObjects/MovingPlatform.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.0001f)
+        {
+            ++currentWaypointIndex;
+                if(currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
