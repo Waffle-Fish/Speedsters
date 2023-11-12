ade1d9dc529ef32d634bdc2f13c053f3abec18de
Josue <xJaramis@gmail.com>
2023-11-11 22:21:45 -0800

    CharacterSelect mostly done

--- Assets/Scripts/Menu/mainMenu.cs
+++ Assets/Scripts/Menu/mainMenu.cs
@@ -1,24 +1,43 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [Min(0)]
-    public int sceneIndex;
-    public void PlayGame(int sceneIndex)
+    private int sceneIndex;
+
+    public bool isPlayer2;
+
+    private void PlayGame()
     {
         Debug.Log("Lets play!");
-        if(sceneIndex >= SceneManager.sceneCountInBuildSettings) { Debug.LogError("Trying to load a scene outside the current number of scenes in the build settings"); }
+        if (sceneIndex >= SceneManager.sceneCountInBuildSettings) { Debug.LogError("Trying to load a scene outside the current number of scenes in the build settings"); }
         SceneManager.LoadScene(sceneIndex);
     }
 
-    //Make sure to know what scene index the levels areS
+    //Function used in LevelSelect (Reference to the MainMenu script should only be made form characterSelects script)
     public void SetIndex(int index)
     {
         sceneIndex = index;
+    }
+
+    //Checks if both players are ready if 2P mode, otherwise, checks if player is ready
+    public void checkPlayerReady()
+    {
+        if (isPlayer2)
+        {
+            if (ReadyButton.Instance.p1ready && ReadyButton.Instance.p2ready)
+            {
+                PlayGame();
+            } 
+        }
+        else
+        {
+            if (ReadyButton.Instance.p1ready) 
+            { 
+                PlayGame(); 
+            }
+        }
     }
 
     public void QuitGame()
