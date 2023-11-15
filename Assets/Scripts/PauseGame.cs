906ffcfd1364377b9071e07f235ed70b167c41bb
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-11-14 17:29:58 -0800

    Add timeDelay function

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -7,7 +7,13 @@ public class PauseGame : MonoBehaviour
 {
     public static PauseGame Instance { get; private set; }
     public bool isGamePaused = false;
+
+    [SerializeField]
+    [Tooltip("Adds more time to the game being paused between exiting the pause menu and resuming play of the game")]
+    private float resumeDelayTime = 0f;
     float prevTimeScale = 0f;
+
+
     private void Awake()
     {
         if (Instance == null) { Instance = this; }
@@ -22,7 +28,13 @@ public class PauseGame : MonoBehaviour
 
     public void Resume()
     {
+        StartCoroutine(ResumeGameDelay());
         Time.timeScale = prevTimeScale;
         isGamePaused = false;
+    }
+
+    private IEnumerator ResumeGameDelay()
+    {
+        yield return new WaitForSeconds(resumeDelayTime);
     }
 }
