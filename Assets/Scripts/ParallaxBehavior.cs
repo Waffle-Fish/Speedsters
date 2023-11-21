8914a32f82ff5f06fbe9c842a5aeeabd2ecc6d92
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-11-21 15:53:49 -0800

    Implement parallax background for top player

---
+++ Assets/Scripts/ParallaxBehavior.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class ParallaxBehavior : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    [SerializeField]
+    Transform playerTrans;
+    [SerializeField]
+    float yOffest = 0f;
+    
+    Vector2 startPosition;
+    float startZ;
+
+    Vector2 Travel => new(cam.transform.position.x - startPosition.x, cam.transform.position.y + yOffest - startPosition.y);
+    float DistanceFromSubject => transform.position.z - playerTrans.position.z;
+    float ClippingPlane => (cam.transform.position.z + (DistanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+    float ParrallaxFactor => Mathf.Abs(DistanceFromSubject) / ClippingPlane;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    void Update()
+    {
+        Vector2 newPos = startPosition + Travel * ParrallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
