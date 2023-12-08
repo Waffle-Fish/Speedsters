fb853d41477e9fdd40da0004918fa821d55945b1
Jhoshua Ampo <ampojhoshua@gmail.com>
2023-12-07 19:26:26 -0800

    Update postlevelprocess

--- Assets/Scripts/PostLevelProcess.cs
+++ Assets/Scripts/PostLevelProcess.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PostLevelProcess : MonoBehaviour
 {
@@ -9,8 +11,10 @@ public class PostLevelProcess : MonoBehaviour
     float initialDelay = 0f;
     [SerializeField]
     GameObject PlayerTextGO;
-    [SerializeField]
-
+    public Sprite p1Sprite;
+    public Sprite p2Sprite;
+    public UnityEngine.UI.Image winnerImg;
+    public UnityEngine.UI.Image loserImg;
     public static PostLevelProcess Instance { get; private set; }
     List<RectTransform> rectTransforms = new();
     private void Awake()
@@ -25,15 +29,21 @@ public class PostLevelProcess : MonoBehaviour
             rectTransforms[i].gameObject.SetActive(false);
         }
     }
-
     public IEnumerator StartProcess() {
         yield return new WaitForSecondsRealtime(initialDelay); // Can add a slight delay between player finishing and finish screen
         PauseGame.Instance.Pause();
         TextMeshProUGUI PlayerTMP = PlayerTextGO.GetComponent<TextMeshProUGUI>();
         PlayerTMP.text = string.Format("Player {0}!",Time_UI.Instance.GetWinner());
+        SetPodiumPictures();
         for (int i = 1; i < rectTransforms.Count; i++)
         {
             rectTransforms[i].gameObject.SetActive(true);
         }
-    }   
+    }  
+
+    private void SetPodiumPictures()
+    {
+        winnerImg.sprite = (Time_UI.Instance.GetWinner() == '1') ? p1Sprite : p2Sprite;
+        loserImg.sprite = (Time_UI.Instance.GetWinner() == '1') ? p2Sprite : p1Sprite;
+    }
 }
--- Assets/winner.cs
+++ Assets/winner.cs
@@ -9,8 +9,10 @@ public class winner : MonoBehaviour
 {
     public Image winnerImage;
     Sprite player1Sprite = Player1Master.Instance.GetComponentsInChildren<SpriteRenderer>()[1].sprite;
+    Sprite player2Sprite = Player2Master.Instance.GetComponentsInChildren<SpriteRenderer>()[1].sprite;
     void Start()
     {
-        winnerImage.sprite = player1Sprite;
+        winnerImage = GetComponent<Image>();
+        winnerImage.sprite = (Time_UI.Instance.GetWinner() == '1') ? player1Sprite : player2Sprite;
     }
 }
